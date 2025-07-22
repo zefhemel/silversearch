37cc9762138b0d93082e13e4bbe681ca4697a788
MrMugame <mr.mugame@gmail.com>
2025-07-23 00:50:53 +0200

    Migrate from zod to valibot

--- worker/util/settings.ts
+++ worker/util/settings.ts
@@ -1,51 +1,53 @@
 import { editor, system } from "@silverbulletmd/silverbullet/syscalls";
-import { z } from "zod"
+import * as v from "@valibot/valibot"
 import { RecencyCutoff } from "./global.ts";
 
 let errorWasShown = false;
 let settings: null | SilversearchSettings = null;
 
-const weightSchema = z.strictObject({
-    content: z.optional(z.number()).default(1),
-    basename: z.optional(z.number()).default(10),
-    displayName: z.optional(z.number()).default(10),
-    directory: z.optional(z.number()).default(7),
-    aliases: z.optional(z.number()).default(1),
-    tags: z.optional(z.number()).default(1),
+const weightSchema = v.strictObject({
+    content: v.optional(v.number(), 1),
+    basename: v.optional(v.number(), 10),
+    displayName: v.optional(v.number(), 10),
+    directory: v.optional(v.number(), 7),
+    aliases: v.optional(v.number(), 1),
+    tags: v.optional(v.number(), 1),
 });
 
-const settingsSchema = z.strictObject({
-    weights: z.optional(weightSchema).default(weightSchema.parse({})),
-    weightCustomProperties: z.optional(z.record(z.string(), z.number())).default({}),
-    recencyBoost: z.optional(z.enum(RecencyCutoff)).default(RecencyCutoff.Disabled),
-    downrankedFoldersFilters: z.optional(z.array(z.string())).default([]),
-    ignoreDiacritics: z.optional(z.boolean()).default(true),
-    ignoreArabicDiacritics: z.optional(z.boolean()).default(false),
-    tokenizeUrls: z.optional(z.boolean()).default(true),
-    splitCamelCase: z.optional(z.boolean()).default(true),
-    fuzziness: z.optional(z.enum(["0", "1", "2"])).default("1"),
-    renderLineReturnInExcerpts: z.optional(z.boolean()).default(true),
+const settingsSchema = v.strictObject({
+    weights: v.optional(weightSchema, v.parse(weightSchema, {})),
+    weightCustomProperties: v.optional(v.record(v.string(), v.number()), {}),
+    recencyBoost: v.optional(v.enum(RecencyCutoff), RecencyCutoff.Disabled),
+    downrankedFoldersFilters: v.optional(v.array(v.string()), []),
+    ignoreDiacritics: v.optional(v.boolean(), true),
+    ignoreArabicDiacritics: v.optional(v.boolean(), false),
+    tokenizeUrls: v.optional(v.boolean(), true),
+    splitCamelCase: v.optional(v.boolean(), true),
+    fuzziness: v.optional(v.picklist(["0", "1", "2"]), "1"),
+    renderLineReturnInExcerpts: v.optional(v.boolean(), true),
 });
 
-export type SilversearchSettings = z.infer<typeof settingsSchema>
+export type SilversearchSettings = v.InferOutput<typeof settingsSchema>
 
 export async function getPlugConfig(): Promise<SilversearchSettings> {
     if (settings) return settings;
 
     const config = await system.getConfig("silversearch", {});
 
-    const result = settingsSchema.safeParse(config);
+    const result = v.safeParse(settingsSchema, config);
 
     if (!result.success) {
         if (!errorWasShown) {
-            await editor.flashNotification(`Silersearch - There was an error in your CONFIG: ${z.prettifyError(result.error)}`);
+            const message = Object.entries(v.flatten<typeof settingsSchema>(result.issues).nested).reduce((acc, [location, err]) => `${acc}; ${err.join(" & ")} in "${location}"`, "").slice(1);
+
+            await editor.flashNotification(`Silersearch - There was an error in your CONFIG: ${message}`);
 
             errorWasShown = true;
         }
 
-        settings = settingsSchema.parse({});
+        settings = v.parse(settingsSchema, {});
     } else {
-        settings = result.data;
+        settings = result.output;
     }
 
     return settings;
