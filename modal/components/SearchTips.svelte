679d7eac868764a393f134f6610ae979151753b2
MrMugame <mr.mugame@gmail.com>
2025-09-30 00:15:24 +0200

    Improve search tips

--- modal/components/SearchTips.svelte
+++ modal/components/SearchTips.svelte
@@ -1,22 +1,23 @@
 <script lang="ts"></script>
 
 <div class="silversearch-help">
-    <p>Some Advanced Tips:</p>
+    <p>Add the following to your query to refine the search:</p>
     <ul>
         <li>
-            Use <code>path:&quot;&lt;somepath&gt;&quot;</code> to restrict your results
-            to corresponding paths
+            Use <code>path:&quot;Library&quot;</code> to restrict your results
+            to paths which include &quot;Library&quot;
         </li>
         <li>
-            Use <code>ext:&quot;png jpg&quot;</code> or <code>ext:png</code>, or
-            a plain <code>.png</code> to specify the filetype(s)
+            Use <code>ext:pdf,jpg</code> or <code>ext:png</code>, or
+            a plain <code>.png</code> to restrict your search to certain filetypes
         </li>
         <li>
             Use <code>&quot;exact expressions&quot;</code> in quotes to further filter
             the results returned by the query
         </li>
         <li>
-            Use <code>-exclusions</code> to exclude notes containing certain words
+            Use <code>-apple -pie</code> to exclude notes containing the words
+            &quot;apple&quot; and &quot;pie&quot;
         </li>
     </ul>
 </div>
